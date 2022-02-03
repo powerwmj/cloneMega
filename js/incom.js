9e77ce4b9f8ca0b25c32f948d47fb62fd6e54556
Yuuuk94 <95343748+Yuuuk94@users.noreply.github.com>
2022-02-03 22:36:42 +0900

    Delete js directory

--- js/incom.js
+++
@@ -1,7 +0,0 @@
-// header, footer를 위한 js 파일입니다! 여기말고 다른 js파일을 만들어서 작업해주세요~
-$('body')
-.prepend('<header></header>')
-.append('<footer></footer>');
-
-$('header').load('incom.html header >div');
-$('footer').load('incom.html footer >div');
