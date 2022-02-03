06e1622500656372721c2f9724c3169b8236e4e1
Yuuuk94 <yuuuk94@gmail.com>
2022-02-03 22:31:25 +0900

    20220203yk

--- js/incom.js
+++ js/incom.js
@@ -1,7 +1,43 @@
-// header, footer를 위한 js 파일입니다! 여기말고 다른 js파일을 만들어서 작업해주세요~
-$('body')
-.prepend('<header></header>')
-.append('<footer></footer>');
+// header, footer 불러오기
+const elBody = document.querySelector('body');
+const elHeader = document.createElement('header');
+const elFooter = document.createElement('footer');
 
-$('header').load('incom.html header >div');
+elBody.prepend(elHeader);
+elBody.append(elFooter);
+
+// header events
+let afterHead = function(){
+    const elBurger = document.querySelector('.bugermenu');
+    const elBp = document.querySelector('.bugermenu p');
+    const elMy = document.querySelector('.my');
+    const elSearch = document.querySelector('.search');
+    // burgermenu
+    elBurger.onclick = function(){
+        elBp.classList.toggle('active');
+        elMy.classList.remove('active');
+        elSearch.classList.remove('active');
+
+    }
+    // my
+    elMy.onclick = function(){
+        elMy.classList.toggle('active');
+        elBp.classList.remove('active');
+        elSearch.classList.remove('active');
+    }
+    // search
+    elSearch.onclick = function(){
+        elSearch.classList.toggle('active');
+        elMy.classList.remove('active');
+        elBp.classList.remove('active');
+    }
+}
+
+$('header').load('incom.html header >div', afterHead);
 $('footer').load('incom.html footer >div');
+//fetch 이용하기
+
+
+
+
+
